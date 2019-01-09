0e9e868add281de64047642d1ce42a95934907a2
Brodie Balser <balserbrodie@gmail.com>
2019-01-08 19:40:11 -0700

    set up new project

---
+++ NewWorldsOfRandomThing/sketch.js
@@ -0,0 +1,19 @@
+
+function setup () {
+	// //find appropriate canvas width
+	// canvas_size = windowWidth < windowHeight ? windowWidth : windowHeight
+	// canvas_size -= 80
+
+	createCanvas( windowWidth, windowHeight)
+
+	// //set canvas in center
+	// let margin_left = (windowWidth - canvas_size) / 2
+	// $(".p5Canvas").css("margin-left", margin_left)
+	// $(".p5Canvas").css("margin-top", 40)
+
+}
+
+
+function draw () {
+
+}

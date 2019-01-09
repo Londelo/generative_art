bd8cede8135917a2629a1ea9a5b62f148fdd168d
Brodie Balser <balserbrodie@gmail.com>
2019-01-08 20:07:28 -0700

    got ground going

--- NewWorldsOfRandomThing/sketch.js
+++ NewWorldsOfRandomThing/sketch.js
@@ -1,19 +1,62 @@
+let TheGround
 
 function setup () {
 	// //find appropriate canvas width
 	// canvas_size = windowWidth < windowHeight ? windowWidth : windowHeight
 	// canvas_size -= 80
 
-	createCanvas( windowWidth, windowHeight)
+	createCanvas( windowWidth, windowHeight )
 
 	// //set canvas in center
 	// let margin_left = (windowWidth - canvas_size) / 2
 	// $(".p5Canvas").css("margin-left", margin_left)
 	// $(".p5Canvas").css("margin-top", 40)
+	TheGround = new ground
 
+	TheGround.size = {
+		w: 1000,
+		h: 100
+	}
+	TheGround.position = {
+		x: 0,
+		y: windowHeight - TheGround.size.h
+	}
 }
 
 
 function draw () {
 
+	background("grey")
+
+	console.log(keyCode);
+	TheGround.draw()
+
+	if(keyCode === 100) {
+		console.log("hit");
+		TheGround.position.x += TheGround.speed
+	}
+	if(keyCode === 97) {
+		TheGround.position.x -= TheGround.speed
+	}
+
 }
+
+function ground () {
+
+	this.postion = {}
+	this.size = {}
+	this.speed = 5
+
+	this.draw = () => {
+
+		fill("black")
+		rect(this.position.x, this.position.y, this.size.w, this.size.h)
+	}
+}
+
+
+
+
+
+
+//

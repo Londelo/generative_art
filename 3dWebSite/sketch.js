2b3fbe35726aacb968fdcd0b3c964e992792dbb5
Brodie Balser <balserbrodie@gmail.com>
2019-02-18 21:22:00 -0700

    its kinda working with all axis now

--- 3dWebSite/sketch.js
+++ 3dWebSite/sketch.js
@@ -25,126 +25,138 @@ function draw () {
 
 function BigBox () {
 
-	this.Pos = { x:0, y:0, z:-1000, rotation: 0 }
+	this.Pos = { x:0, y:0, z:-1000, rotation: 0, axis:"x" }
 	this.Size = { w:windowWidth, h:windowHeight, d:windowHeight }
-	this.OrigPos = {
-		rotation: -1,
-		front: {
+	this.OrigPos = [
+		{//front
 			x: this.Pos.x - (this.Size.w/2),
 			y: this.Pos.y - (this.Size.h/2),
 			z: this.Pos.z + (this.Size.d/2),
-		},
-		top:{
+		},{//top
 			x: this.Pos.x - (this.Size.w/2),
 			y: this.Pos.y - (this.Size.h/2),
 			z: this.Pos.z - (this.Size.d/2),
-		},
-		back:{
+		},{//back
 			x: this.Pos.x - (this.Size.w/2),
 			y: this.Pos.y + (this.Size.h/2),
 			z: this.Pos.z - (this.Size.d/2),
-		},
-		bottom:{
+		},{//bottom
 			x: this.Pos.x - (this.Size.w/2),
 			y: this.Pos.y + (this.Size.h/2),
 			z: this.Pos.z + (this.Size.d/2),
-		},
-		left:{
+		},{//left
 			x: this.Pos.x - (this.Size.w/2),
 			y: this.Pos.y - (this.Size.h/2),
 			z: this.Pos.z - (this.Size.d/2),
-		},
-		right:{
+		},{//right
 			x: this.Pos.x + (this.Size.w/2),
 			y: this.Pos.y - (this.Size.h/2),
 			z: this.Pos.z + (this.Size.d/2),
 		}
-	}
-	this.Faces = {1:{},2:{},3:{},4:{},5:{},6:{}}
+	]
+	this.Faces = this.OrigPos
 
 	this.Draw = () => {
+		// console.table(this.Faces);
+		// console.log("------------------");
+		// console.table(this.OrigPos);
 
 		push()
 		fill("red")
 		translate(this.Pos.x, this.Pos.y, this.Pos.z)
-		rotateZ(this.Pos.rotation)
-  	box(this.Size.w, this.Size.h, this.Size.d,0,0);
+
+		if(this.Pos.axis === "x") {
+			rotateX(this.Pos.rotation)
+		}
+		if(this.Pos.axis === "y") {
+			rotateY(this.Pos.rotation)
+		}
+		if(this.Pos.axis === "z") {
+			rotateZ(this.Pos.rotation)
+		}
+
+  	box(this.Size.w, this.Size.h, this.Size.d)
 		pop()
 	}
 
 	this.Rotate = () => {
 
 		if(key === "w") {
+			this.Pos.rotation = 0
 			this.Pos.rotation += .05
+			this.Pos.axis = "x"
+			rotateOnX()
+		}
+		if(key === "d") {
+			this.Pos.rotation = 0
+			this.Pos.rotation += .05
+			this.Pos.axis = "y"
+			rotateOnY()
+		}
+		if(key === "a") {
+			this.Pos.rotation = 0
+			this.Pos.rotation += .05
+			this.Pos.axis = "z"
+			rotateOnZ()
 		}
 	}
 
-	let SetFaces = () => {
+	let rotateOnZ = () => {
 
 		let cosA = Math.cos(this.Pos.rotation), sinA = Math.sin(this.Pos.rotation)
 
-		this.Faces = {
+		let newFaces = this.Faces.map((Face, i) => {
 
-			1:{
-				pos: {
-					x: this.OrigPos.front.x * cosA - this.OrigPos.front.y * sinA,
-					y: this.OrigPos.front.y * cosA + this.OrigPos.front.x * sinA,
-					z: this.OrigPos.front.z,
-				}
-			},
-			2:{
-				pos: {
-					x: this.OrigPos.top.x * cosA - this.OrigPos.top.y * sinA,
-					y: this.OrigPos.top.y * cosA + this.OrigPos.top.x * sinA,
-					z: this.OrigPos.top.z,
-				}
-			},
-			3:{
-				pos: {
-					x: this.OrigPos.back.x * cosA - this.OrigPos.back.y * sinA,
-					y: this.OrigPos.back.y * cosA + this.OrigPos.back.x * sinA,
-					z: this.OrigPos.back.z,
-				}
-			},
-			4:{
-				pos: {
-					x: this.OrigPos.bottom.x * cosA - this.OrigPos.bottom.y * sinA,
-					y: this.OrigPos.bottom.y * cosA + this.OrigPos.bottom.x * sinA,
-					z: this.OrigPos.bottom.z,
-				}
-			},
-			5:{
-				pos: {
-					x: this.OrigPos.left.x * cosA - this.OrigPos.left.y * sinA,
-					y: this.OrigPos.left.y * cosA + this.OrigPos.left.x * sinA,
-					z: this.OrigPos.left.z,
-				}
-			},
-			6:{
-				pos: {
-					x: this.OrigPos.right.x * cosA - this.OrigPos.right.y * sinA,
-					y: this.OrigPos.right.y * cosA + this.OrigPos.right.x * sinA,
-					z: this.OrigPos.right.z,
-				}
+			return {
+				x: this.OrigPos[i].x * cosA - this.OrigPos[i].y * sinA,
+				y: this.OrigPos[i].y * cosA + this.OrigPos[i].x * sinA,
+				z: this.OrigPos[i].z
 			}
-		}
+		})
+
+		this.Faces = newFaces
 	}
 
-	let SetOrigPos = () => {
-		this.OrigPos.rotation = this.Pos.rotation
+	let rotateOnY = () => {
+
+		let cosA = Math.cos(this.Pos.rotation), sinA = Math.sin(this.Pos.rotation)
+
+		let newFaces = this.Faces.map((Face, i) => {
+
+			return {
+				x: this.OrigPos[i].x * cosA + this.OrigPos[i].z * sinA,
+				y: this.OrigPos[i].y,
+				z: this.OrigPos[i].z * cosA - this.OrigPos[i].x * sinA
+			}
+		})
+
+		this.Faces = newFaces
+	}
+
+	let rotateOnX = () => {
+
+		let cosA = Math.cos(this.Pos.rotation), sinA = Math.sin(this.Pos.rotation)
+
+		let newFaces = this.Faces.map((Face, i) => {
+
+			return {
+				x: this.OrigPos[i].x,
+				y: this.OrigPos[i].y * cosA - this.OrigPos[i].z * sinA,
+				z: this.OrigPos[i].z * cosA + this.OrigPos[i].y * sinA
+			}
+		})
+
+		this.Faces = newFaces
 	}
 
 	this.DrawDots = () => {
 
-		if(this.Pos.rotation !== this.OrigPos.rotation) {
-			SetFaces()
-			SetOrigPos()
-		}
 
-		for (let i = 1; i <= 6; i++) {
+		for (let i = 0; i < 6; i++) {
+			let Face = this.Faces[i]
 			push()
 			fill("black")
-			translate(this.Faces[i].pos.x, this.Faces[i].pos.y, this.Faces[i].pos.z)
+			translate(Face.x, Face.y, Face.z)
 			sphere(20);
 			pop()
 		}

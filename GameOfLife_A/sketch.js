5ee12e7709bacd5012f41cf89d11ff43e02c4a48
TM_Brodie <brodie.balser@ticketmaster.com>
2023-01-05 17:32:36 -0700

    made progress on the game of life

--- GameOfLife_A/sketch.js
+++ GameOfLife_A/sketch.js
@@ -1,10 +1,138 @@
-	let state
-	function State() {
-		this.cells = []
-		this.cell_size = 10
-		this.framesCounted = 0
-		this.importantDifference = 0
+const State = {
+	cells: [],
+	cellSize: 10,
+	framesCounted: 0,
+	gridW: 600,
+	gridH: 600,
+	freeze: false,
+	lifeSpeed: 5
+}
+
+const Cell = {
+	position: { x: 0, y: 0 },
+	size: State.cellSize,
+	hasLife: false,
+	age: 1
+}
+
+function setup () {
+
+	createCanvas( windowWidth, windowHeight )
+	noStroke()
+
+	createGrid()
+	startLife()
+}
+
+function draw () {
+	State.framesCounted ++
+
+	if(State.framesCounted === State.lifeSpeed) {
+		lifeHappens()
+		State.framesCounted = 0
 	}
+
+	if(!State.freeze) {
+		background("black")
+		for (let index = 0; index < State.cells.length; index++) {
+			let cell = State.cells[index];
+			drawCell(cell)
+		}
+	}
+}
+
+function createGrid () {
+	const position = { x: 0, y: 0 }
+	const {
+		gridH,
+		gridW,
+		cellSize
+	} = State
+
+	while(position.y < gridH) {
+
+		const newCell = {
+			...Cell,
+			position: { ...position }
+		}
+		State.cells.push(newCell)
+
+		if(position.x < (gridW - cellSize)) {
+			position.x += cellSize
+		} else {
+			position.y += cellSize
+			position.x = 0
+		}
+	}
+}
+
+function startLife () {
+	const numOfFirstArivals = 1000
+	for (let index = 0; index < numOfFirstArivals; index++) {
+		let randomIndex = random(500, 800).toFixed(0)
+		const cell = State.cells[randomIndex ];
+		cell.hasLife = true
+	}
+}
+
+function drawCell (cell, test) {
+	stroke("black")
+
+	if(cell.hasLife) {
+		fill(`rgba(0,255,0, ${cell.age})`)
+	} else {
+		fill("white")
+	}
+
+	if(test) {
+		fill('red')
+	}
+
+	square(cell.position.x, cell.position.y, cell.size)
+}
+
+function countLivingNeighbors (cellIndex) {
+
+	const {
+		cells,
+		gridW,
+		cellSize
+	} = State
+
+	const numOfRows = gridW/cellSize
+
+	const left = cells[cellIndex - 1]
+	const right = cells[cellIndex + 1]
+	const topMiddle = cells[cellIndex - numOfRows]
+	const topRight = cells[(cellIndex - numOfRows) + 1]
+	const topLeft = cells[(cellIndex - numOfRows) - 1]
+	const bottomMiddle = cells[(cellIndex + numOfRows)]
+	const bottomRight = cells[(cellIndex + numOfRows) + 1]
+	const bottomLeft = cells[(cellIndex + numOfRows) - 1]
+
+	const arrayOfCells = [ 
+		left,
+		right,
+		topMiddle,
+		topRight,
+		topLeft,
+		bottomMiddle,
+		bottomLeft,
+		bottomRight
+	]
+
+	let numOfAliveNeighbors = 0
+
+	arrayOfCells.forEach(cell => {
+		if(cell && cell.hasLife) {
+			numOfAliveNeighbors++
+		}
+	})
+
+	return numOfAliveNeighbors
+}
+
+function lifeHappens() {
 
 	//THE RULES
 
@@ -13,187 +141,27 @@
 	// Any live cell with two or three live neighbours lives, unchanged, to the next generation
 	// Any dead cell with exactly three live neighbours will come to life
 
-	function setup () {
+	const {
+		cells
+	} = State
 
-		state = new State()
+	for (let index = 0; index < cells.length; index++) {
+		const cell = cells[index]
+		const numOfAliveNeighbors = countLivingNeighbors(index)
 
-		createCanvas( windowWidth, windowHeight )
-		noStroke()
-
-		CellSetup()
-	}
-
-	function draw () {
-		background("black")
-		state.framesCounted ++
-
-		if(state.framesCounted === 200) {
-			console.log("LIFE BABY")
-			Life()
-			state.framesCounted = 0
-		}
-
-		for (let index = 0; index < state.cells.length; index++) {
-			let cell = state.cells[index];
-
-			cell.draw()
-		}
-	}
-
-	function Cell (position, size) {
-		this.position = position
-		this.size = size
-		this.hasLife = false
-		this.yearsOld = 1
-
-		this.draw = () => {
-			stroke("black")
-
-			if(this.hasLife) {
-				fill(`rgba(0,255,0, ${this.yearsOld})`)
-			} else {
-				fill("white")
+		if(cell.hasLife) {
+			if(numOfAliveNeighbors < 2 || numOfAliveNeighbors > 3) {
+				cell.hasLife = false
+			} else if (cell.age > 0.1) {
+				cell.age -= 0.1
 			}
-			square(this.position.x, this.position.y, this.size)
-		}
-	}
-
-	function CellSetup () {
-		let position = { x: 0, y: 0 },
-		gridW = 300,
-		gridH = 300,
-		cell_size = state.cell_size
-
-		while(position.y < gridH) {
-			let cell = new Cell({...position}, cell_size)
-			state.cells.push(cell)
-
-			if(position.x > gridW) {
-				position.y += cell_size
-				position.x = 0
-			} else {
-				position.x += cell_size
-			}
-		}
-
-		for (let index = 0; index < 20; index++) {
-			let randomIndex = random(500, 800).toFixed(0)
-			const cell = state.cells[randomIndex];
-			cell.hasLife = true
-		}
-
-		CheckImportantDifference()
-
-		// state.cells[200].hasLife = true
-		// state.cells[200 - (state.importantDifference - 1)].hasLife = true
-		// state.cells[200 - state.importantDifference].hasLife = true
-	}
-
-	function CheckImportantDifference () {
-		let centerIndex = 200, topCenterIndex = 0 
-		for (let index = 0; index < state.cells.length; index++) {
-			let cell = state.cells[index],
-			centerCell = state.cells[centerIndex].position
-
-			// if(JSON.stringify(cell.position) === JSON.stringify({x: centerCell.x - state.cell_size, y: centerCell.y + state.cell_size})) {
-			// 	console.log("top left", cell.position, index)
-			// 	cell.hasLife = true
-			// }
-
-			if(JSON.stringify(cell.position) === JSON.stringify({x: centerCell.x, y: centerCell.y + state.cell_size})) {
-				console.log("top center", cell.position, index)
-				// cell.hasLife = true
-				topCenterIndex = index
-			}
-
-			// if(JSON.stringify(cell.position) === JSON.stringify({x: centerCell.x + state.cell_size, y: centerCell.y + state.cell_size})) {
-			// 	console.log("top right", cell.position, index)
-			// 	cell.hasLife = true
-			// }
-
-			// if(JSON.stringify(cell.position) === JSON.stringify({x: centerCell.x - state.cell_size, y: centerCell.y})) {
-			// 	console.log("middle left", cell.position, index)
-			// 	cell.hasLife = true
-			// }
-
-			// if(JSON.stringify(cell.position) === JSON.stringify(centerCell)) {
-			// 	console.log("middle CENTER", cell.position, index)
-			// 	cell.hasLife = true
-			// }
-
-			// if(JSON.stringify(cell.position) === JSON.stringify({x: centerCell.x + state.cell_size, y: centerCell.y})) {
-			// 	console.log("middle right", cell.position, index)
-			// 	cell.hasLife = true
-			// }
-
-			// if(JSON.stringify(cell.position) === JSON.stringify({x: centerCell.x - state.cell_size, y: centerCell.y - state.cell_size})) {
-			// 	console.log("bottom left", cell.position, index)
-			// 	cell.hasLife = true
-			// }
-
-			// if(JSON.stringify(cell.position) === JSON.stringify({x: centerCell.x, y: centerCell.y - state.cell_size})) {
-			// 	console.log("bottom center", cell.position, index)
-			// 	cell.hasLife = true
-			// }
-
-			// if(JSON.stringify(cell.position) === JSON.stringify({x: centerCell.x + state.cell_size, y: centerCell.y - state.cell_size})) {
-			// 	console.log("bottom right", cell.position, index)
-			// 	cell.hasLife = true
-			// }
-
-		}
-		state.importantDifference = topCenterIndex - centerIndex
-		console.log(state.importantDifference, topCenterIndex, centerIndex)
-	}
-
-	function Life() {
-		for (let index = 0; index < state.cells.length; index++) {
-			let numberOfNeightbors = 0
-
-			let topRight = state.cells[index - (state.importantDifference + 1)] ? state.cells[index - (state.importantDifference + 1)] : null,
-			topCenter = state.cells[index - state.importantDifference] ? state.cells[index - state.importantDifference] : null,
-			topLeft = state.cells[index - (state.importantDifference - 1)] ? state.cells[index - (state.importantDifference - 1)] : null,
-			left = state.cells[index - 1] ? state.cells[index - 1] : null,
-			center = state.cells[index],
-			right = state.cells[index + 1] ? state.cells[index + 1] : null,
-			bottomRight = state.cells[index + (state.importantDifference + 1)] ? state.cells[index + (state.importantDifference + 1)] : null,
-			bottomCenter = state.cells[index + state.importantDifference] ? state.cells[index + state.importantDifference] : null,
-			bottomLeft = state.cells[index + (state.importantDifference - 1)] ? state.cells[index + (state.importantDifference - 1)] : null
-
-			let arrayOfCells = [ 
-				topRight, topCenter, topLeft,
-				left, center, right,
-				bottomLeft, bottomCenter, bottomRight
-			]
-
-			for (let index = 0; index < arrayOfCells.length; index++) {
-				const cell = arrayOfCells[index];
-				
-				if(cell) {
-					if(cell.hasLife && JSON.stringify(cell.position) !== JSON.stringify(center.position)) {
-						numberOfNeightbors++
-					}
-				}
-			}
-
-			if(center.hasLife && numberOfNeightbors < 2) {
-				center.hasLife = false
-			}
-
-			if(center.hasLife &&numberOfNeightbors > 3) {
-				center.hasLife = false
-			}
-
-			if(center.hasLife && (numberOfNeightbors === 3 || numberOfNeightbors === 2)) {
-				center.yearsOld -= 0.05
-				center.yearsOld = center.yearsOld.toFixed(2)
-			}
-
-			if(!center.hasLife && numberOfNeightbors === 3) {
-				center.hasLife = true
-				center.yearsOld = 1
+		} else {
+			if(numOfAliveNeighbors === 3) {
+				cell.hasLife = true
+				cell.age = 1
 			}
 		}
 	}
+}
 
-	
+
